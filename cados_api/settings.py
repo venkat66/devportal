45f0ea0660169cf06f05494a346700bdf0879270
venkat66 <venkatashivaji979@gmail.com>
2022-12-08 21:37:43 +0530

    seconddd

--- cados_api/settings.py
+++ cados_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,12 +126,25 @@ WSGI_APPLICATION = 'cados_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgress',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'postgresql://postgres:B1no2yueUkhwq1XW3kzm@containers-us-west-121.railway.app:6438/railway',
+        'PORT': 6438,
     }
 }
+
+db_from_env = dj_database_url.config(default='postgres://localhost')
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
