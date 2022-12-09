39388bd255dfb574d789d61208a53139367c26b9
venkat66 <venkatashivaji979@gmail.com>
2022-12-09 16:27:23 +0530

    database

--- cados_api/settings.py
+++ cados_api/settings.py
@@ -126,13 +126,23 @@ WSGI_APPLICATION = 'cados_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgressql',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':''
     }
 }
-
 
 # db_from_env = dj_database_url.config(default='postgres://localhost')
 # DATABASES['default'].update(db_from_env)
